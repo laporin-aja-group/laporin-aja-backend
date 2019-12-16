1c3014a034b763ba340afc6213857438504e8bce
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2019-12-16 09:39:21 +0700

    change on getByProcess

--- routes/reports/index.js
+++ routes/reports/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router()
 router.get("/", require("./controller").getAllRports);
 router.get("/id/:id", require("./controller").getById);
 router.get("/email/:email", require("./controller").getByEmail);
-router.get("/:email/:process", require("./controller").getByProcess);
+router.get("/process/:email/:process", require("./controller").getByProcess);
 router.post("/", require("./controller").addReports);
 router.delete("/:id", require("./controller").deleteOne);
 router.put("/:id", require("./controller").updateOne);
